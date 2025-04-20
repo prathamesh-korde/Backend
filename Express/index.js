82c0c6fb42207ed3a18a970fac1fea40394de7a2
prathamesh-korde <kordeprathamesh01@gmail.com>
2025-04-20 20:42:20 +0530

    Express

--- Express/index.js
+++ Express/index.js
@@ -5,3 +5,23 @@ console.dir(app);
 app.listen(port,()=>{
     console.log(`app is listening on port ${port}`)
 });
+app.use((req,res)=>{
+    console.log("request recived");
+   // console.log(req); //(express automatically convert text message into an object)
+   // console.log(res);
+   /*
+   
+   res.send({
+    "name":"prathamesh",
+    "branch":"IT",
+   });
+
+   */
+
+
+   let HTMLCode="<h1>Fruits</h1><ul><li>orange</li><li>apple</li><li>banana</li></ul>";
+   res.send(HTMLCode);
+
+   
+
+})
