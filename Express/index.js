0d2982dd7c19b6176e041583c730cb274b08c146
prathamesh-korde <kordeprathamesh01@gmail.com>
2025-04-20 21:44:41 +0530

    routing

--- Express/index.js
+++ Express/index.js
@@ -5,23 +5,42 @@ console.dir(app);
 app.listen(port,()=>{
     console.log(`app is listening on port ${port}`)
 });
+
+
+/*
 app.use((req,res)=>{
     console.log("request recived");
    // console.log(req); //(express automatically convert text message into an object)
    // console.log(res);
-   /*
    
-   res.send({
-    "name":"prathamesh",
-    "branch":"IT",
-   });
+  // res.send({
+  //  "name":"prathamesh",
+   // "branch":"IT",
+  // });
 
-   */
-
+   
+//can't send multiple responce for same path
 
    let HTMLCode="<h1>Fruits</h1><ul><li>orange</li><li>apple</li><li>banana</li></ul>";
    res.send(HTMLCode);
-
-   
-
 })
+   */
+
+
+app.get("/",(req,res)=>{
+    res.send("you can contacted root path");
+});
+
+app.get("/orange",(req,res)=>{
+    res.send("you can contacted orange path");
+});
+
+app.get("/apple",(req,res)=>{
+    res.send("you can contacted apple path");
+});
+
+app.get("/banana",(req,res)=>{
+    res.send("you can contacted banana path");
+});
+
+
