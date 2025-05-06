402bea21a98be3c79ee70837b7c4382645266d7d
prathamesh-korde <kordeprathamesh01@gmail.com>
2025-05-06 10:08:45 +0530

    js oop's

--- Miscellaneous/Frontend/app.js
+++ Miscellaneous/Frontend/app.js
@@ -17,14 +17,60 @@ let p2=personmaker("pritesh",20);
 */
 
 //constructer - dos't return anything and start with capital letter
+/*
 function Person(name,age){
     this.name=name;
-    this.age=age;
-    
+    this.age = age;
 }
-Person.prototype.talk=function (){
-    console.log(`hi, my name is ${this.name}`);
+
+Person.prototype.talk=function(){
+    console.log(`hii my name is ${this.name}`);
 };
 
-let p1=new Person("pratham",21);
-let p2=new Person("pritesh",20);
+p1 = new Person("ajay",21);
+
+*/
+/*
+Classes-
+class Person{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+    talk(){
+        console.log(`hi my name is ${this.name}`);
+    }
+}
+
+p1=new Person("prtham",21);
+p2=new Person("pran",21);
+*/
+
+// Inheritance
+class Person{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+    talk(){
+        console.log(`hii my name is ${this.name}`);
+    }
+}
+
+class student extends Person{
+    constructor(name,age,mark){
+        super(name,age);
+        this.mark=mark;
+    }
+}
+
+s1=new student("pratham",21,99);
+
+class Teacher extends Person{
+    constructor(name,age,sub){
+        super(name,age);
+        this.sub=sub;
+    }
+}
+
+t1=new Teacher("adam",40,"math");
