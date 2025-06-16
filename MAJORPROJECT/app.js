329a4d18f16b8a891644e2d4e5bd1370da74147e
prathamesh-korde <kordeprathamesh01@gmail.com>
2025-06-16 23:22:42 +0530

    implementing Rest api

--- MAJORPROJECT/app.js
+++ MAJORPROJECT/app.js
@@ -2,22 +2,70 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const mongo_URL = "mongodb://127.0.0.1:27017/NxtStay";
+const Listing = require("./models/listing.js");
+const path = require("path");
 
-
-main().then(()=>{
-    console.log("connected to DB")
-}).catch(err=>{
+main()
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
 async function main() {
-    await mongoose.connect(mongo_URL);
+  await mongoose.connect(mongo_URL);
 }
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
 
-app.get("/",(req,res)=>{
-    res.send("app is working");
+app.use(express.json()); 
+
+app.get("/", (req, res) => {
+  res.send("App is working");
+});
+
+
+//new route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
 })
 
-app.listen(8080,()=>{
-    console.log("server listning on port 8080");
+//index route
+app.get("/Listings",async (req,res)=>{
+   const allListings = await Listing.find({})
+  res.render("listings/index.ejs", { allListings });
 })
+
+//create
+app.post("/listings", async (req, res) => {
+ let listing = res.body.listing;
+ console.log(listing);
+
+});
+
+
+/*
+app.get("/testListing", async (req, res) => {
+  try {
+    let sampleListing = new Listing({
+      title: "My New Villa",
+      description: "By the beach", 
+      price: 6000, 
+      location: "goa",
+      country: "India",
+    });
+
+    await sampleListing.save();
+    console.log("Sample was saved");
+    res.send("Successful testing");
+  } catch (err) {
+    console.error("Error saving listing:", err);
+    res.status(500).send("Something went wrong");
+  }
+});
+*/
+
+app.listen(8080, () => {
+  console.log("Server listening on port 8080");
+});
--- MAJORPROJECT/init/index.js
+++ MAJORPROJECT/init/index.js
@@ -1,3 +1,25 @@
 const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
 
-const data = require("./data.js");
+const mongo_URL = "mongodb://127.0.0.1:27017/NxtStay";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(mongo_URL);
+}
+
+const initDB = async () =>{
+  await Listing.deleteMany({});
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+}
+
+initDB();
